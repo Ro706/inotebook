14d2e190357a4abf5e6f086cc65c57775cf7bacb
Ro706 <rohitmandal99091@gmail.com>
2025-03-28 03:57:41 +0530

    add page (About,home,pagenotfound)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Pagenotfound from './components/Pagenotfound';
 function App() {
   return (
-    <div className="App">
-     <h1>Welcome To Inotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Pagenotfound />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
