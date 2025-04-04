ba0bd9f8f2f538d3fb991e0715db6fc7003f8a0b
Ro706 <rohitmandal99091@gmail.com>
2025-04-04 23:49:01 +0530

    context api update feature effect

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useContext } from 'react'
 import noteContext from '../context/notes/noteContext'
 export default function About() {
   const a = useContext(noteContext)
-  console.log(a)
+  useEffect(() => {
+    a.update()
+  }, [])
   return (
     <div>
-      <h1>This is About {a.name}</h1>
+      <h1>This is About {a.state.name} and class is {a.state.class}</h1>
     </div>
   )
 }
--- src/context/notes/NoteStates.js
+++ src/context/notes/NoteStates.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { useState } from "react";
 import { NoteProvider } from "./noteContext";
 
 const NoteState = (props) => {
-    const state = {
+    const s1 = {
         name: "Rohit",
         class: "5b"
     };
-    
+    const [state, setState] = useState(s1);
+    const update = () => {
+        setTimeout(() => {
+            setState({
+                name: "Rohit kumar Mandal",
+                class: "6b"
+            });
+        }, 5000);
+    };
     return (
-        <NoteProvider value={state}>
+        <NoteProvider value={{ state, update }}>
             {props.children}
         </NoteProvider>
     );
